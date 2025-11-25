ad609ff38179592aa776c9a6aba830fd0f1b769c
Leonard758 <74091332+Leonard758@users.noreply.github.com>
2025-11-25 15:40:37 +0100

    Primo commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php
+    include 'components/header.html';
+    ?>
+
+</body>
+
+</html>
