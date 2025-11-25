72ae2e69682fbc34c0043d868f4bbc28fc43d764
AleZane29 <alessio.zanella29@gmail.com>
2025-11-25 08:38:28 +0100

    Aggiunta navbar

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style/style.css" rel="stylesheet">
+</head>
+
+<?php
+
+session_start();
+include 'components/header.html';
+?>
+
+<body>
+    
+</body>
+
+
+</html>
