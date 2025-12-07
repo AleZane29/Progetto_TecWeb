1a9c3267db954797de18075aeaff3af42bee6653
AleZane29 <alessio.zanella29@gmail.com>
2025-12-07 17:51:19 +0100

    Fix login

--- views/login.php
+++ views/login.php
@@ -29,7 +29,7 @@
 
 
         <?php if (!empty($error)) {
-          echo ("<p class='error-message' role='alert'>$errore</p>");
+          echo ("<p class='error-message' role='alert'>$error</p>");
         } else {
           if (!empty($success)) {
             echo ("<p class='success-message' role='alert'>$success</p>");
