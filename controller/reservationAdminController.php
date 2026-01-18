11e6eb2316d58271e182203e30e5f59aab3432bd
Leonard758 <74091332+Leonard758@users.noreply.github.com>
2026-01-18 20:10:48 +0100

    risoluzione accessibilitá e stile/html su home e servizi

--- controller/reservationAdminController.php
+++ controller/reservationAdminController.php
@@ -34,8 +34,8 @@ if ($connessioneOK) {
                         <td>" . $res["prezzo"] . "€</td>
                         <td>
                             <div class='actions'>
-                                <button class='action-btn btn-edit' onclick='openEditDialog(" . $res["id"] . ", this)'>Modifica</button>
-                                <button class='action-btn btn-delete' onclick='openDeleteDialog(" . $res["id"] . ")'>Elimina</button>
+                                <button class='action-btn btn-edit' role='button' onclick='openEditDialog(" . $res["id"] . ", this)'>Modifica</button>
+                                <button class='action-btn btn-delete' role='button' yonclick='openDeleteDialog(" . $res["id"] . ")'>Elimina</button>
                             </div>
                         </td>
                     </tr>";
