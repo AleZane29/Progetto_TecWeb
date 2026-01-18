55148db15248b23464e4f545495b30db5fd047e3
AleZane29 <alessio.zanella29@gmail.com>
2026-01-18 11:31:20 +0100

    Corretto SQL

--- SportLab.sql
+++ SportLab.sql
@@ -43,10 +43,8 @@ CREATE TABLE Prenotazione (
 
 
 INSERT INTO Utente (nome, cognome, email, data_nascita, password, ruolo) VALUES
-('User', 'User', 'user@email.com', '1990-01-01', '$2y$10$Br3zzgw8yid4tWA906YI8OPGRaeAjAU6GFjVi9GHdVu6GA2MXY00m', 'Cliente'),
-('Admin', 'Admin', 'admin@email.com', '1985-05-10', '$2y$10$7z0CA06KVMGqMUCaD8HfAOP.wHDdk6pkcyCN8r3DTK5IbCcCOOVMK', 'Admin');
--- psw di admin: Admin
--- psw di user: User
+('User', 'User', 'user@email.com', '1990-01-01', '$2y$10$VbcD96fPgs3zxKy0GxRg9.gEaFQ2.kNGRl9n3OoLP0aVG8VbdbDyO', 'Cliente'),
+('Admin', 'Admin', 'admin@email.com', '1985-05-10', '$2y$10$HOmpkThH3.WDpI9OmDqW3uU/NYq7p9JhT3maUZg5QjIM1HH.Shivm', 'Admin');
 
 INSERT INTO TipoCampo (nome, costo_orario) VALUES
 ('Tennis', 25),
@@ -59,4 +57,4 @@ INSERT INTO Campo (numero, tipo) VALUES
 (3, 'Tennis'),
 (1, 'Calcio5'),
 (2, 'Calcio5'),
-(2, 'Basket');
+(1, 'Basket');
