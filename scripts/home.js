e574d8b901b6f0b698fe7e8eca00d88a143b07f1
AleZane29 <alessio.zanella29@gmail.com>
2026-01-20 15:22:09 +0100

    Correzioni accessibilità

--- scripts/home.js
+++ scripts/home.js
@@ -14,7 +14,6 @@ document.getElementById('navMenuHome').classList.add('active');
 	function showSlides(n) {
 		let i;
 		let slides = document.getElementsByClassName('mySlides');
-		let dots = document.getElementsByClassName('dot');
 		if (n > slides.length) {
 			slideIndex = 1;
 		}
@@ -24,9 +23,5 @@ document.getElementById('navMenuHome').classList.add('active');
 		for (i = 0; i < slides.length; i++) {
 			slides[i].style.display = 'none';
 		}
-		for (i = 0; i < dots.length; i++) {
-			dots[i].className = dots[i].className.replace(' actual', '');
-		}
 		slides[slideIndex - 1].style.display = 'block';
-		dots[slideIndex - 1].className += ' actual';
 	}
